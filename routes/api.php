9bd3f50b6933b9df9c6aa6958b09ef26d5eb2e50
Rafael Nuansa <iandrewnew@gmail.com>
2024-07-06 09:04:59 +0700

    route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+use App\Http\Controllers\Api\PostController;
+
+Route::get('posts', [PostController::class, 'index']);
+Route::post('posts', [PostController::class, 'store']);
+Route::get('posts/{id}', [PostController::class, 'show']);
+Route::put('posts/{id}', [PostController::class, 'update']);
+Route::delete('posts/{id}', [PostController::class, 'destroy']);
